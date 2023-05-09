3429dadc1119b7bb9e22f00aea350361105be478
Ravil Khusainov <ravil@partof.team>
2023-05-09 14:51:14 +0300

    Request public init added

--- Sources/OpenAI/API+Request.swift
+++ Sources/OpenAI/API+Request.swift
@@ -7,5 +7,10 @@ public extension API {
         
         public let model: Model
         public let messages: [Message]
+        
+        public init(model: Model = .gpt3_5, messages: [Message]) {
+            self.model = model
+            self.messages = messages
+        }
     }
 }
